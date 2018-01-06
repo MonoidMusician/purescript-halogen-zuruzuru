8374e87bcffa6917f1ca7cf562dfa436c83678a1
Nicholas Scheel <monoidmusician@gmail.com>
2018-01-05 18:35:23 -0600

    Okay, drag and drop works

--- src/Halogen/DnD.purs
+++ src/Halogen/DnD.purs
@@ -3,23 +3,40 @@ module Halogen.DnD where
 
 import Prelude
 
+import Control.Apply (lift2)
+import Control.Monad.Aff.Class (class MonadAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.AVar (AVAR)
 import Control.Monad.Eff.Class (class MonadEff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Ref (REF)
+import Control.Monad.Except (runExcept)
 import Control.Monad.Maybe.Trans (MaybeT(..), runMaybeT)
+import Control.Monad.State (class MonadState)
+import Control.MonadZero (guard)
 import DOM (DOM)
-import Data.Array (deleteAt, insertAt, length, updateAt, (!!))
+import DOM.Event.Event (target)
+import DOM.Event.Types (Event, MouseEvent)
+import DOM.HTML.Event.Types (DragEvent)
+import DOM.HTML.HTMLElement (getBoundingClientRect)
+import DOM.HTML.HTMLInputElement (value)
+import DOM.HTML.Types (readHTMLElement)
+import Data.Array (deleteAt, findIndex, findLastIndex, insertAt, length, updateAt, (!!))
+import Data.Either (hush)
 import Data.FoldableWithIndex (foldMapWithIndex)
-import Data.Lens (Lens', view, (%=), (.=))
+import Data.Foreign (toForeign)
+import Data.Lens (Lens', Traversal', _Just, preview, view, (%=), (+=), (.=))
 import Data.Lens.Record (prop)
-import Data.Maybe (Maybe(Nothing), fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe, isNothing, maybe)
 import Data.Monoid (class Monoid, mempty)
 import Data.Symbol (SProxy(..))
+import Data.Traversable (traverse)
+import Data.TraversableWithIndex (traverseWithIndex)
+import Halogen (AttrName(..), SubscribeStatus(..))
 import Halogen as H
 import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.Component.Utils.Drag as Drag
 import Halogen.HTML as HH
 import Halogen.HTML.Elements.Keyed as HK
 import Halogen.HTML.Events as HE
@@ -32,18 +49,36 @@ data Query a
   | Add Int a
   | Remove Int a
   | Swap Int Int a
-  | Dragging Int a
+  | Dragging Int MouseEvent a
+  | Move Drag.DragEvent a
+  | DragTo Int a
+  | Update Int String a
 
 type State =
   { values :: Array String
-  , dragging :: Maybe
-    { index :: Int
-    , displacement :: Number
-    }
+  , dragging :: Maybe DragState
   }
+
+type DragState =
+  { index :: Int
+  , offset :: Number
+  , displacement :: Number
+  }
+
+fresh :: forall s m. MonadState s m => Lens' s Int -> m Int
+fresh lens = H.gets (view lens) <* H.modify (lens (_+1))
 
 _values :: Lens' State (Array String)
 _values = prop (SProxy :: SProxy "values")
+
+_dragging :: Lens' State (Maybe DragState)
+_dragging = prop (SProxy :: SProxy "dragging")
+
+_dragIndex :: Traversal' State Int
+_dragIndex = _dragging <<< _Just <<< prop (SProxy :: SProxy "index")
+
+_offset :: Traversal' State Number
+_offset = _dragging <<< _Just <<< prop (SProxy :: SProxy "offset")
 
 initialState :: Array String -> State
 initialState = { values: _, dragging: Nothing }
@@ -57,7 +92,7 @@ surroundMapWithIndices m f as = (_ <> m (length as)) $
     m i <> f i a
 
 dnd :: forall m a q eff.
-  MonadEff ( dom :: DOM, console :: CONSOLE | eff ) m =>
+  MonadAff ( dom :: DOM, console :: CONSOLE, avar :: AVAR, ref :: REF | eff ) m =>
   H.Component HH.HTML Query (Array String) q m
 dnd =
   H.lifecycleComponent
@@ -78,22 +113,29 @@ dnd =
 
     add = HH.div_ [ but "+" (Add 0) ]
     adding i = [ HH.div_ [ but "+" (Add i) ] ]
-    dragging i = HH.button
-      [ HE.onMouseDown (HE.input_ (Dragging i)) ]
+    handle i = HH.button
+      [ HE.onMouseDown (HE.input (Dragging i))
+      , HP.attr (AttrName "style") "cursor: move"
+      ]
       [ HH.text "≡" ]
+    dragStyle :: forall r i. Maybe DragState -> Int -> HP.IProp r i
+    dragStyle dragging idx = HP.attr (H.AttrName "style") case dragging of
+      Just { index: i, displacement, offset } | i == idx ->
+        "transform: translateY(" <> show (displacement - offset) <> "px)"
+      _ -> ""
 
     render :: State -> H.ComponentHTML Query
-    render { values } = HH.div_ $ values #
+    render { values, dragging } = HH.div_ $ values #
       surroundMapWithIndices adding \i v ->
-        [ HH.div_
+        [ HH.div [ dragStyle dragging i ]
           [ but' (i > 0) "▲" (Swap i (i-1))
-          , dragging i
+          , handle i
           , but' (i < length values - 1) "▼" (Swap i (i+1))
           , HH.text (" " <> show (i+1) <> ". ")
           , HH.input
             [ HP.ref (label i)
             , HP.value v
-            -- , HE.onInput (HE.input (Update <<< id))
+            , HE.onValueInput (HE.input (Update i <<< id))
             -- , HE.onClick (HE.input (Update <<< mouseEventToEvent))
             -- , HE.onKeyUp (HE.input (Update <<< keyboardEventToEvent))
             -- , HE.onFocus (HE.input (Update <<< focusEventToEvent))
@@ -102,29 +144,98 @@ dnd =
           ]
         ]
 
+    mid = _.top `lift2 (+)` _.bottom >>> (_ / 2.0)
+
+    getPos :: Int -> MaybeT (H.ComponentDSL State Query q m) Number
+    getPos i = do
+      e <- MaybeT $ H.getRef (label i)
+      mid <$> H.liftEff (getBoundingClientRect (unsafeCoerce e))
+
+    getPoses :: H.ComponentDSL State Query q m (Array (Maybe Number))
+    getPoses =
+      H.gets (view _values) >>=
+        traverseWithIndex \i _ ->
+          runMaybeT $ getPos i
+          {-
+          runMaybeT do
+            e <- MaybeT $ H.getRef (label i)
+            m <- mid <$> H.liftEff (getBoundingClientRect (unsafeCoerce e))
+            v <- H.liftEff (value (unsafeCoerce e))
+            pure {pos:m,val:v}
+          -}
+
     eval :: Query ~> H.ComponentDSL State Query q m
     eval (Set values next) = next <$ do
       _values .= values
+    eval (Update i v next) = next <$ do
+      _values %= (fromMaybe <*> updateAt i v)
     eval (Add i next) = next <$ do
+      -- getPoses >>= H.liftEff <<< log <<< unsafeCoerce
       _values %= (fromMaybe <*> insertAt i mempty)
+      -- getPoses >>= H.liftEff <<< log <<< unsafeCoerce
     eval (Remove i next) = next <$ do
+      -- getPoses >>= H.liftEff <<< log <<< unsafeCoerce
       _values %= (fromMaybe <*> deleteAt i)
+      -- getPoses >>= H.liftEff <<< log <<< unsafeCoerce
     eval (Swap i j next) = next <$ runMaybeT do
       values <- H.lift $ H.gets (view _values)
       a <- MaybeT $ pure (values !! i)
       b <- MaybeT $ pure (values !! j)
       v' <- MaybeT $ pure (updateAt i b values)
       v'' <- MaybeT $ pure (updateAt j a v')
+      -- H.lift $ getPoses >>= H.liftEff <<< log <<< unsafeCoerce
       _values .= v''
-    eval (Dragging i next) = next <$ do
-      H.liftEff $ logShow i
+      -- H.lift $ getPoses >>= H.liftEff <<< log <<< unsafeCoerce
+    eval (DragTo i' next) = next <$ runMaybeT do
+      dragging <- MaybeT $ H.gets $ view _dragging
+      let i = dragging.index
+      oldPos <- getPos i
+      values <- H.gets (view _values)
+      v <- MaybeT $ pure (values !! i)
+      v' <- MaybeT $ pure (deleteAt i values)
+      v'' <- MaybeT $ pure (insertAt i' v v')
+      let dragging' = dragging { index = i' }
+      H.put { values: v'', dragging: Just dragging' }
+      newPos <- getPos i
+      _offset += (oldPos - newPos)
+    eval (Dragging i e next) = next <$ runMaybeT do
+      -- H.liftEff $ logShow i
+      H.lift $ H.subscribe $ Drag.dragEventSource e \e -> Just $ Move e Listening
+      _dragging .= Just { index: i, displacement: 0.0, offset: 0.0 }
+    eval (Move e next) = next <$ case e of
+      Drag.Move e d -> do
+        -- H.liftEff $ logShow d.offsetY
+        _dragging %= map _ { displacement = d.offsetY }
+        poses <- getPoses
+        -- H.liftEff $ log $ unsafeCoerce poses
+        void $ runMaybeT do
+          i <- MaybeT $ H.gets $ preview _dragIndex
+          p <- getPos i
+          let
+            -- these will either default to `i` (already) or
+            -- be the index that this should go to
+            least = fromMaybe i $ poses # findIndex (maybe false (_ >= p))
+            most = fromMaybe i $ poses # findLastIndex (maybe false (_ <= p))
+          -- H.liftEff $ logShow [least, i, most]
+          guard (least < most)
+          let
+            i'
+              | most > i = most
+              | least < i = least
+              | otherwise = i
+          guard (i /= i') -- redundant, but just to be safe
+          -- H.liftEff $ logShow i'
+          H.lift $ eval (DragTo i' unit)
+      Drag.Done e -> do
+        -- H.liftEff $ log "Done"
+        _dragging .= Nothing
 
 data DemoQuery a
   = Receive Void a
   | Reset (Array String) a
 
 demo :: forall m eff.
-  MonadEff ( dom :: DOM, console :: CONSOLE | eff ) m =>
+  MonadAff ( dom :: DOM, console :: CONSOLE, avar :: AVAR, ref :: REF | eff ) m =>
   H.Component HH.HTML DemoQuery Unit Void m
 demo =
   H.lifecycleParentComponent
